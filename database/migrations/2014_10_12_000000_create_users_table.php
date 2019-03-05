dc12a1227fe9b51a995d651802555a35f2070b14
jefri <widiantojefri@gmail.com>
2019-03-05 22:41:07 +0700

    update migration for table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('jabatan', ['1', '2']);
             $table->timestamps();
         });
     }
